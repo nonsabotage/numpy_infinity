e69d8dfb01ac460f32b9733ad6be4f9802020206
nonsabotage <nonsabotage@gmail.com>
2021-01-13 12:11:01 +0900

    update

---
+++ README.md
@@ -0,0 +1,20 @@
+# インフニティNumpy
+
+## 概要
+
+- 著者：こしあん
+- URL：https://koshian2.booth.pm/
+- Pythonの基礎からnumpyを使った画像処理まで
+- 演習問題が豊富
+
+## 注意点
+
+- 問題の写し、回答例の写しは公開してはいけない
+- 自分の回答は公開してよい
+
+## 演習問題解答
+
+- [１章　演習問題１](https://colab.research.google.com/drive/1TjmAubnnKPs5Vn_QlZNlx_DH9isjvpkw?usp=sharing)
+
+
+
---
+++ mypy/mymodule.py
@@ -0,0 +1,13 @@
+import time
+class TimeExecute:
+    def __init__(self):
+        pass
+
+    def __enter__(self):
+        self.start_time = time.time()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        exec_time_msec = 1000 * (time.time() - self.start_time)
+        print(f"{exec_time_msec:.3f} [msec]")
+    
